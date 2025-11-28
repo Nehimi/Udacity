b2f33ad368c7c2c678f11978b8fa249032b7a057
Eng Nehimi Gulufa <nehimiagulufa@gmail.com>
2025-11-28 17:52:13 +0300

    Update logic in switch.js to handle new requirements

---
+++ Switch/switch.js
@@ -0,0 +1,30 @@
+var tier = "nsfw deck";
+var output = "You will receive "
+
+switch (tier) {
+  case "deck of legends":
+    output += "a custom card, ";
+  case "collector's deck":
+    output += "a signed version of the Exploding Kittens deck, ";
+  case "nsfw deck":
+    output += "one copy of the NSFW (Not Safe for Work) Exploding Kittens card game and ";
+  default:
+    output += "one copy of the Exploding Kittens card game.";
+}
+
+console.log(output);
+
+// Prints: You’ll receive one copy of the NSFW (Not Safe for Work) Exploding Kittens card game and one copy of the Exploding Kittens card game
+
+var tiers = "none";
+var outputs = "You will receive ";
+
+switch (tiers) {
+ 
+  default:
+    outputs += "one copy of the Exploding Kittens card game.";
+}
+
+console.log(outputs);
+// Prints: You’ll receive one copy of the Exploding Kittens card game.
+
