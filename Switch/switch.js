022575ee7042d4b1b8cf66c7c3c6e1c90b77038d
Eng Nehimi Gulufa <nehimiagulufa@gmail.com>
2025-12-03 15:56:29 +0300

    new example is added

--- Switch/switch.js
+++ Switch/switch.js
@@ -28,3 +28,19 @@ switch (tiers) {
 console.log(outputs);
 // Prints: You’ll receive one copy of the Exploding Kittens card game.
 
+
+var tier = "nsfw deck";
+var output = "You will receive "
+
+switch (tier) {
+  case "deck of legends":
+    output += "a custom card, ";
+  case "collector's deck":
+    output += "a signed version of the Exploding Kittens deck, ";
+  case "nsfw deck": // <-- The switch finds a match here!
+    output += "one copy of the NSFW (Not Safe for Work) Exploding Kittens card game and ";
+  default: // <-- It then 'falls through' and executes the default block too.
+    output += "one copy of the Exploding Kittens card game.";
+}
+
+console.log(output);
