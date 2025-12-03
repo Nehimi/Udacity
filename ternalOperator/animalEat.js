7fbd057cfc8eb03300d87a774b2e30600c19577b
Eng Nehimi Gulufa <nehimiagulufa@gmail.com>
2025-12-03 15:49:56 +0300

    nested ef else

--- ternalOperator/animalEat.js
+++ ternalOperator/animalEat.js
@@ -13,3 +13,21 @@ var eatsAnimals = true;
 var category = eatsPlants ? (eatsAnimals ? "omnivore" : "herbivore") : (eatsAnimals ? "carnivore" : undefined);
 
 console.log(category);
+
+if (eatsPlants) {
+    if (eatsAnimals) {
+        // (true, true)
+        category = "omnivore";
+    } else {
+        // (true, false)
+        category = "herbivore";
+    }
+} else {
+    if (eatsAnimals) {
+        // (false, true)
+        category = "carnivore";
+    } else {
+        // (false, false)
+        category = "undefined";
+    }
+}
