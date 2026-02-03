13f12e69f0365e35a60d4e65047f39c95d86af29
Developer nehimi <nehimiagulufa@gmail.com>
2026-02-03 11:19:11 +0300

    object of js

---
+++ Function/object.js
@@ -0,0 +1,9 @@
+let stocks = {//object
+  fruits: ["strawber", "banana", "graps"],
+
+  vegetables: ["tomato", "potato", "onion", "carrot", "cabbage"]
+};
+
+console.log(stocks.vegetables);
+console.log(stocks.vegetables[4]);
+
