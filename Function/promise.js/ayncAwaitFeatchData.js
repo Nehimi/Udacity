9507b482e16fec41b3dee9c0281e42a275a03ab6
Developer nehimi <nehimiagulufa@gmail.com>
2026-02-04 11:10:45 +0300

    aditional featrure

--- Function/promise.js/ayncAwaitFeatchData.js
+++ Function/promise.js/ayncAwaitFeatchData.js
@@ -3,7 +3,11 @@ async function getData(){
       let url="https://jsonplaceholder.typicode.com/users";
    let response=await fetch(url);
    let data= await response.json();
-   console.log(data[0].address.geo); 
+   // console.log(data[0].address.geo); 
+//       If you want ONE property from ALL users
+// Example: get all names
+let names = data.map(user => user.name);
+console.log(names);
    }
    catch(err){
 console.log("Error:",err)
@@ -11,3 +15,4 @@ console.log("Error:",err)
   
 }
 getData();
+
